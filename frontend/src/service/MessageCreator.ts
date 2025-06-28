e2f9982972aae9bf37897d628195974564583091
Norman Böttcher <norman.boettcher@web.de>
2025-06-28 15:28:46 +0200

    Added - Unit Test for MessageCreator

--- frontend/src/service/MessageCreator.ts
+++ frontend/src/service/MessageCreator.ts
@@ -2,12 +2,22 @@ import type {ChatMessageType} from "../domain/ChatMessage.types.ts";
 import type {BotResponse} from "../domain/BotResponse.ts";
 
 const useMessageCreator = () => {
+
+    //@ts-ignore
+    const dateOptions: Inc.DateTimeFormatOptions = {
+        day: '2-digit',
+        month: "2-digit",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+    }
+
     const createChatMessage = (botResponse: BotResponse) => {
-        const date: Date = new Date();
+        const date = new Date().toLocaleDateString("de-DE", dateOptions);
         const chatMessage: ChatMessageType = {
             sender: botResponse.sender,
             message: botResponse.text,
-            timestamp: `${date.getDay() + 1}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`,
+            timestamp: date
         };
         return chatMessage;
     };
