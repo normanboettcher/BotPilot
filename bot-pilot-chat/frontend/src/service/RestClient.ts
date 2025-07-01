c8ba03926515ec5f4910f200da9bcc20df2d0c95
Norman Böttcher <norman.boettcher@web.de>
2025-07-01 05:50:54 +0200

    Changed - post request RestClient

--- bot-pilot-chat/frontend/src/service/RestClient.ts
+++ bot-pilot-chat/frontend/src/service/RestClient.ts
@@ -3,7 +3,14 @@ import type { Query } from "../domain/Query";
 const useRestClient = () => {
   const send = async (query: Query) => {
     try {
-      return await fetch(`http://localhost:8000/api/faqs/${query}`);
+      return await fetch(`http://localhost:8000/api/faqs`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(query),
+      });
     } catch (error) {
       console.log("Error when sending the question to Backend Server", error);
     }
