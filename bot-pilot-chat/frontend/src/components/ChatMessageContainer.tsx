c88cba29c1e928d6c7719a94a0557c03ee117dcc
Norman Böttcher <norman@fedora.fritz.box>
2025-07-07 20:14:09 +0200

    Changed - switched directions of stack in ChatMessageContainer

--- bot-pilot-chat/frontend/src/components/ChatMessageContainer.tsx
+++ bot-pilot-chat/frontend/src/components/ChatMessageContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SenderAvatar from "./SenderAvatar.tsx";
 import type { ChatMessageType } from "../domain/ChatMessage.ts";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import ChatMessage from "./ChatMessage.tsx";
 
 type Props = {
@@ -11,10 +11,16 @@ type Props = {
 const ChatMessageContainer: React.FC<Props> = ({ chatMessage }) => {
   const { sender, message } = chatMessage;
   return (
-    <Stack direction={sender === "bot" ? "row-reverse" : "row"} spacing={1}>
-      <SenderAvatar sender={sender} />
-      <ChatMessage sender={sender} text={message} />
-    </Stack>
+    <Box
+      width={"100%"}
+      display={"flex"}
+      justifyContent={sender === "user" ? "flex-end" : "flex-start"}
+    >
+      <Stack direction={sender === "user" ? "row-reverse" : "row"} spacing={1}>
+        <SenderAvatar sender={sender} />
+        <ChatMessage sender={sender} text={message} />
+      </Stack>
+    </Box>
   );
 };
 
