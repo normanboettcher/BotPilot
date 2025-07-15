765269c3595d11b4985be5c6f997a6ff2deda7eb
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-07-15 05:54:51 +0200

    Added - updateKontaktdaten function to KontaktdatenForm

--- bot-pilot-web/chatme-web-app/src/components/form/pilot/KontaktdatenForm.vue
+++ bot-pilot-web/chatme-web-app/src/components/form/pilot/KontaktdatenForm.vue
@@ -1,30 +1,58 @@
 <script setup lang="ts">
-import { defineModel } from 'vue'
-import type { Kontaktdaten } from '@/components/domain/Kontaktdaten.ts'
-import PilotFormTextField from '@/components/form/pilot/PilotFormTextField.vue'
+import type { Kontaktdaten } from '@/components/domain/Kontaktdaten.ts';
+import PilotFormTextField from '@/components/form/pilot/PilotFormTextField.vue';
+import { defineProps, defineEmits, computed } from 'vue';
+import { isStringInputEmpty } from '@/components/form/pilot/utils/pilotform.utils.ts';
 
-const modelValue = defineModel<Kontaktdaten>({
-  type: Object,
-  default: () => ({
-    ansprechpartner: '',
-    email: '',
-    telefon: '',
-    anmerkungen: '',
-    kanzlei: '',
-  }),
-})
+const props = defineProps<{
+  modelValue: Kontaktdaten;
+}>();
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: Kontaktdaten): void;
+}>();
+
+const updateKontaktdaten = (field: keyof Kontaktdaten, value: string | undefined) => {
+  if (isStringInputEmpty(value)) {
+    return;
+  }
+  const newValue = { ...props.modelValue, [field]: value };
+  emit('update:modelValue', newValue);
+};
+
+const kontaktdaten = computed(() => props.modelValue);
 </script>
 
 <template>
-  <pilot-form-text-field id="kanzlei" v-model="modelValue.kanzlei" label="Kanzlei" />
+  <pilot-form-text-field
+    id="kanzlei"
+    @update:model-value="(value) => updateKontaktdaten('kanzlei', value)"
+    :model-value="kontaktdaten.kanzlei"
+    label="Kanzlei"
+  />
   <pilot-form-text-field
     id="ansprechpartner"
-    v-model="modelValue.ansprechpartner"
+    @update:model-value="(value) => updateKontaktdaten('ansprechpartner', value)"
+    v-model="kontaktdaten.ansprechpartner"
     label="Ansprechpartner"
   />
-  <pilot-form-text-field v-model="modelValue.email" label="E-Mail" type="email" />
-  <pilot-form-text-field v-model="modelValue.telefon" label="Telefonnummer" type="tel" />
-  <v-textarea v-model="modelValue.anmerkungen" label="Anmerkungen:" outlined />
+  <pilot-form-text-field
+    v-model="kontaktdaten.email"
+    label="E-Mail"
+    @update:model-value="(value) => updateKontaktdaten('email', value)"
+    type="email"
+  />
+  <pilot-form-text-field
+    v-model="kontaktdaten.telefon"
+    @update:model-value="(value) => updateKontaktdaten('telefon', value)"
+    label="Telefonnummer"
+    type="tel"
+  />
+  <v-textarea
+    v-model="kontaktdaten.anmerkungen"
+    @update:model-value="(value) => updateKontaktdaten('anmerkungen', value)"
+    label="Anmerkungen:"
+    outlined
+  />
 </template>
 
 <style scoped>
