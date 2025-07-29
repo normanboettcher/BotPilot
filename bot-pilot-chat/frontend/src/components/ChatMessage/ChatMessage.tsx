bd2ce2c7532547642ab76751efa8ece4691710b2
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-07-29 08:19:06 +0200

    Small styling changes in ChatMessage

--- bot-pilot-chat/frontend/src/components/ChatMessage/ChatMessage.tsx
+++ bot-pilot-chat/frontend/src/components/ChatMessage/ChatMessage.tsx
@@ -1,38 +1,37 @@
 import React from "react";
-import type { Sender } from "../../domain/Sender.ts";
-import LargeChatMessageComponent from "./LargeChatMessageComponent.tsx";
-import SmallChatMessageComponent from "./SmallChatMessageComponent.tsx";
+import type {Sender} from "../../domain/Sender.ts";
+import {Box, Typography} from "@mui/material";
 
 type Props = {
-  text: string;
-  sender: Sender;
+    text: string;
+    sender: Sender;
 };
 
-const ChatMessage: React.FC<Props> = ({ text, sender }) => {
-  const isUser = sender === "user";
-  // Farben & Styles je nach Absender
-  const backgroundColor = isUser ? "primary.main" : "grey.200";
-  const textColor = isUser ? "white" : "black";
-  const align = isUser ? "flex-end" : "flex-start";
-  const useChip = text.length > 50;
-  if (useChip) {
+const ChatMessage: React.FC<Props> = ({text, sender}) => {
+    const isUser = sender === "user";
+    // Farben & Styles je nach Absender
+    const backgroundColor = isUser ? "primary.main" : "grey.200";
+    const textColor = isUser ? "white" : "black";
+    const align = isUser ? "flex-end" : "flex-start";
     return (
-      <SmallChatMessageComponent
-        align={align}
-        backgroundColor={backgroundColor}
-        textColor={textColor}
-        text={text}
-      />
-    );
-  }
-  return (
-    <LargeChatMessageComponent
-      align={align}
-      backgroundColor={backgroundColor}
-      textColor={textColor}
-      text={text}
-    ></LargeChatMessageComponent>
-  );
+        <Box
+            sx={{
+                display: 'flex',
+                alignText: align,
+                backgroundColor,
+                color: textColor,
+                borderRadius: '16px',
+                borderTopLeftRadius: sender === "bot" ? "0px" : "16px",
+                borderRadiusTopRight: sender === "user" ? "0px" : "16px",
+                p: 1.5,
+                m: 0.5,
+                minWidth: "70%",
+                maxWidth: "70%",
+            }}
+        >
+            <Typography variant={'body2'} pb={1} pt={0}>{text}</Typography>
+        </Box>
+    )
 };
 
 export default ChatMessage;
--- bot-pilot-chat/frontend/src/components/ChatMessage/ChatMessageComponent.types.ts
+++ bot-pilot-chat/frontend/src/components/ChatMessage/ChatMessageComponent.types.ts
@@ -1,6 +1,9 @@
+import type {Sender} from "../../domain/Sender.ts";
+
 export interface ChatMessageComponentProps {
   backgroundColor: string;
   textColor: string;
   align: "flex-start" | "flex-end";
   text: string;
+  sender: Sender;
 }
