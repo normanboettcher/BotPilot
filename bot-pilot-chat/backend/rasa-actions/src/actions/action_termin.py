3e459649214c903adb13fe6e5fbad233029d9844
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-08-17 17:53:33 +0200

    Added - entities for termin

--- bot-pilot-chat/backend/rasa-actions/src/actions/action_termin.py
+++ bot-pilot-chat/backend/rasa-actions/src/actions/action_termin.py
@@ -1,6 +1,10 @@
+import logging
+
 from bot_pilot_chat.domain.response import BotResponse
 from rasa_sdk import Action
 from ..utils.response_wrapper import send_response
+
+logger = logging.getLogger(__name__)
 
 
 class ActionTermin(Action):
@@ -8,6 +12,8 @@ class ActionTermin(Action):
         return "action_termin"
 
     def run(self, dispatcher, tracker, domain):
+        logger.debug(f"observed entities: {tracker.latest_message['entities']}")
+        logger.debug(f"latest message: {tracker.latest_message}")
         general_message = (
             "Ich habe das intent Termin erkannt. Implementierung folgt."
         )
