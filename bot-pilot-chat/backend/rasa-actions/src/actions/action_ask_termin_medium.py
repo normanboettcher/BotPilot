a893092c4b1643c2412dca39bbae51fa4c5d786b
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-08-22 20:34:36 +0200

    small changes in rules

--- bot-pilot-chat/backend/rasa-actions/src/actions/action_ask_termin_medium.py
+++ bot-pilot-chat/backend/rasa-actions/src/actions/action_ask_termin_medium.py
@@ -14,26 +14,32 @@ class ActionAskTerminMedium(Action):
         return "action_ask_termin_medium"
 
     def run(self, dispatcher, tracker, domain):
-        attempts = tracker.slots.get('termin_medium_attempts')
-        logger.debug('attempts: %s', attempts)
+        attempts = tracker.slots.get("termin_medium_attempts")
+        logger.debug("attempts: %s", attempts)
         if attempts == 0:
-            message = ('Welchen Medien wollen Sie benutzen, um Ihnen einen '
-                       'Termin zu bestimmen? Email oder Telefon?')
+            message = (
+                "Welchen Medien wollen Sie benutzen, um Ihnen einen "
+                "Termin zu bestimmen? Email oder Telefon?"
+            )
             res = BotResponse.with_answer(message)
             dispatcher.utter_message(json_message=send_response(res.as_dict()))
-            return [SlotSet('termin_medium_attempts', 1)]
+            return [SlotSet("termin_medium_attempts", 1)]
         if 3 > attempts > 0:
-            message = ('Es tut mir leid, ich konnte leider nicht verstehen, '
-                       'ob Sie über E-Mail oder Telefon einen Termin '
-                       'anfragen wollen.')
+            message = (
+                "Es tut mir leid, ich konnte leider nicht verstehen, "
+                "ob Sie über E-Mail oder Telefon einen Termin "
+                "anfragen wollen."
+            )
             res = BotResponse.with_answer(message)
             dispatcher.utter_message(json_message=send_response(res.as_dict()))
-            logger.debug('set attempt + 1')
-            return [SlotSet('termin_medium_attempts', attempts + 1)]
+            logger.debug("set attempt + 1")
+            return [SlotSet("termin_medium_attempts", attempts + 1)]
         else:
-            fail_message = ('Es tut mir leid, dass ich Ihre Anfrage gerade '
-                            'nicht verarbeiten konnte.')
+            fail_message = (
+                "Es tut mir leid, dass ich Ihre Anfrage gerade "
+                "nicht verarbeiten konnte."
+            )
             res = BotResponse.with_answer(fail_message)
             dispatcher.utter_message(json_message=send_response(res.as_dict()))
-            logger.debug('too many attempts. reset')
-            return [SlotSet('termin_medium_attempts', 0)]
+            logger.debug("too many attempts. reset")
+            return [SlotSet("termin_medium_attempts", 0)]
