e6dc2a6b48d2dce12a231013e821c8a94086ea45
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-09-17 19:36:52 +0200

    Added - logging for google_calendar business classes

--- bot-pilot-chat/backend/bot-connectors/src/bot_connectors/service/google_calendar_provider.py
+++ bot-pilot-chat/backend/bot-connectors/src/bot_connectors/service/google_calendar_provider.py
@@ -1,7 +1,11 @@
+import logging
+
 from sqlalchemy.orm import Session
 from googleapiclient.discovery import build
 
-from bot_connectors.persistence.google_calendar_das import GoogleCalendarDAS
+from bot_connectors.persistence.google_calendar_das import GoogleCalendarDas
+
+logger = logging.Logger(__name__)
 
 
 class GoogleCalendarProvider:
@@ -10,7 +14,15 @@ class GoogleCalendarProvider:
         self._customer_context = customer_context
 
     def get_google_calendar_service(self):
-        das = GoogleCalendarDAS(self._db_session)
-        calendar_creds = das.get_credentials_for_context(self._customer_context)
-        return build("calendar", "v3",
-                     credentials=calendar_creds)
+        das = GoogleCalendarDas(self._db_session)
+        try:
+            calendar_creds = das.get_credentials_for_context(
+                self._customer_context)
+            logger.debug(f'Received credentials for google-calendar for '
+                         f'[{self._customer_context}] successfully')
+            return build("calendar", "v3",
+                         credentials=calendar_creds)
+        except Exception as e:
+            logger.error(f'Failed to get credentials for '
+                         f'google-calendar for [{self._customer_context}]: {e}')
+            raise
