9108c94d66da498c028926ad2dd527bc76bd6e1f
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-09-27 07:41:09 +0200

    Fixed - Build Errors

--- bot-pilot-chat/frontend/src/main.tsx
+++ bot-pilot-chat/frontend/src/main.tsx
@@ -3,7 +3,6 @@ import './style.css';
 import React from 'react';
 import App from './App.tsx';
 import { ChatContextProvider } from './context/ChatContext.tsx';
-import { CalendarAccessory } from './components/ChatMessage/Accessories/CalendarAccessory';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
