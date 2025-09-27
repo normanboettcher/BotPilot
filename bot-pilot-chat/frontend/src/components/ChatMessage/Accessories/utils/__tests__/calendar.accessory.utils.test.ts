1691b6d983b3903b2466a2acb75a71e2f14651bb
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-09-27 07:36:47 +0200

    Added - Check for opening hours in shouldDisableTime

--- bot-pilot-chat/frontend/src/components/ChatMessage/Accessories/utils/__tests__/calendar.accessory.utils.test.ts
+++ bot-pilot-chat/frontend/src/components/ChatMessage/Accessories/utils/__tests__/calendar.accessory.utils.test.ts
@@ -1,7 +1,10 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import dayjs from 'dayjs';
 import { shouldDisableTime } from '../calendar.accessory.utils.ts';
 import type { BusyEvent } from '../../../../../domain/BusyEvent.ts';
+import * as calendar_details from '../../useCalendarDetails.ts';
+import type { CalendarDetails } from '../../../../../domain/CalendarDetails.ts';
+import { busyEventsMock } from '../../../../mocks/MockBusyEvents.ts';
 
 const givenBusyEvent = (start: string, end: string): BusyEvent => ({
   start: {
@@ -13,6 +16,24 @@ const givenBusyEvent = (start: string, end: string): BusyEvent => ({
     timeZone: 'Europe/Berlin',
   },
 });
+
+const baseCalendarDetails: CalendarDetails = {
+  // all day long
+  openingHours: [
+    {
+      start: {
+        dateTime: '2025-10-02T00:00:00+02:00',
+        timeZone: 'Europe/Berlin',
+      },
+      end: {
+        dateTime: '2025-10-03T00:00:00+02:00',
+        timeZone: 'Europe/Berlin',
+      },
+    },
+  ],
+  busyEvents: busyEventsMock,
+};
+const useCalendarDetailsSpy = vi.spyOn(calendar_details, 'default');
 
 describe('shouldDisableTime', () => {
   const hours = [];
@@ -42,11 +63,18 @@ describe('shouldDisableTime', () => {
       end = `2025-10-02T${hourEnd}:00:00+02:00`;
     }
     const busyEvent = givenBusyEvent(start, end);
+    useCalendarDetailsSpy.mockReturnValue({
+      ...baseCalendarDetails,
+      busyEvents: {
+        ...baseCalendarDetails.busyEvents,
+        busyEvents: [busyEvent],
+      },
+    });
     const selectedDate = dayjs(`2025-10-02T${hourStart}:30:00+02:00`);
     console.log(`Testing with hour start: [${start}] and end: [${end}]`);
 
     // when
-    const result = shouldDisableTime(selectedDate, 'hours', [busyEvent]);
+    const result = shouldDisableTime(selectedDate, 'minutes');
 
     // then
     expect(result).toEqual(true);
@@ -72,11 +100,18 @@ describe('shouldDisableTime', () => {
       const start = `2025-10-02T10:${busyStart}:00+02:00`;
       const end = `2025-10-02T10:${busyEnd}:00+02:00`;
       const busyEvent = givenBusyEvent(start, end);
+      useCalendarDetailsSpy.mockReturnValue({
+        ...baseCalendarDetails,
+        busyEvents: {
+          ...baseCalendarDetails.busyEvents,
+          busyEvents: [busyEvent],
+        },
+      });
 
       const selectedDate = dayjs(`2025-10-02T10:${selectStart}:00+02:00`);
 
       // when
-      const result = shouldDisableTime(selectedDate, 'minutes', [busyEvent]);
+      const result = shouldDisableTime(selectedDate, 'minutes');
 
       // then
       expect(result).toEqual(false);
@@ -98,14 +133,156 @@ describe('shouldDisableTime', () => {
       const start = `2025-10-02T10:${busyStart}:00+02:00`;
       const end = `2025-10-02T10:${busyEnd}:00+02:00`;
       const busyEvent = givenBusyEvent(start, end);
+      useCalendarDetailsSpy.mockReturnValue({
+        ...baseCalendarDetails,
+        busyEvents: {
+          ...baseCalendarDetails.busyEvents,
+          busyEvents: [busyEvent],
+        },
+      });
 
       const selectedDate = dayjs(`2025-10-02T10:${selectStart}:00+02:00`);
 
       // when
-      const result = shouldDisableTime(selectedDate, 'minutes', [busyEvent]);
+      const result = shouldDisableTime(selectedDate, 'minutes');
 
       // then
       expect(result).toEqual(true);
     }
   );
+  it.each([
+    ['2025-10-02T13:00:00+02:00'],
+    ['2025-10-02T13:30:00+02:00'],
+    ['2025-10-02T14:00:00+02:00'],
+    ['2025-10-02T14:15:00+02:00'],
+    ['2025-10-02T14:30:00+02:00'],
+    ['2025-10-02T17:15:00+02:00'],
+    ['2025-10-02T17:30:00+02:00'],
+    ['2025-10-02T18:30:00+02:00'],
+  ])('should return true if time %s is not in opening hours', (time) => {
+    // given
+    const selectedDate = dayjs(time);
+    useCalendarDetailsSpy.mockReturnValue({
+      openingHours: [
+        {
+          start: {
+            dateTime: '2025-10-02T08:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+          end: {
+            dateTime: '2025-10-02T12:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+        },
+        {
+          start: {
+            dateTime: '2025-10-02T15:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+          end: {
+            dateTime: '2025-10-02T17:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+        },
+      ],
+      busyEvents: {
+        timespanDays: 90,
+        busyEvents: [],
+      },
+    });
+
+    // when
+    const result = shouldDisableTime(selectedDate, 'minutes');
+
+    // then
+    expect(result).toEqual(true);
+  });
+  it.each([
+    ['2025-10-02T08:00:00+02:00'],
+    ['2025-10-02T13:00:00+02:00'],
+    ['2025-10-02T13:30:00+02:00'],
+    ['2025-10-02T14:00:00+02:00'],
+    ['2025-10-02T14:15:00+02:00'],
+    ['2025-10-02T14:30:00+02:00'],
+    ['2025-10-02T17:15:00+02:00'],
+    ['2025-10-02T17:30:00+02:00'],
+  ])('should return false if time %s is in opening hours', (time) => {
+    // given
+    const selectedDate = dayjs(time);
+    useCalendarDetailsSpy.mockReturnValue({
+      openingHours: [
+        {
+          start: {
+            dateTime: '2025-10-02T08:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+          end: {
+            dateTime: '2025-10-02T18:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+        },
+      ],
+      busyEvents: {
+        timespanDays: 90,
+        busyEvents: [],
+      },
+    });
+
+    // when
+    const result = shouldDisableTime(selectedDate, 'minutes');
+
+    // then
+    expect(result).toEqual(false);
+  });
+  it('should not allow 18:00 if opening hours are only until 18:00', () => {
+    // given
+    const selectedDate = dayjs('2025-10-02T18:00:00+02:00');
+    useCalendarDetailsSpy.mockReturnValue({
+      openingHours: [
+        {
+          start: {
+            dateTime: '2025-10-02T08:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+          end: {
+            dateTime: '2025-10-02T18:00:00+02:00',
+            timeZone: 'Europe/Berlin',
+          },
+        },
+      ],
+      busyEvents: {
+        timespanDays: 90,
+        busyEvents: [],
+      },
+    });
+
+    // when
+    const result = shouldDisableTime(selectedDate, 'minutes');
+
+    // then
+    expect(result).toEqual(true);
+  });
+  it('should not allow 11:00 if busy event is from 10:15-11:15', () => {
+    // given
+    const selectedDate = dayjs('2025-10-02T11:00:00+02:00');
+
+    useCalendarDetailsSpy.mockReturnValue({
+      ...baseCalendarDetails,
+      busyEvents: {
+        timespanDays: 90,
+        busyEvents: [
+          {
+            start: { dateTime: '2025-10-02T10:15:00+02:00', timeZone: 'Europe/Berlin' },
+            end: { dateTime: '2025-10-02T11:15:00+02:00', timeZone: 'Europe/Berlin' },
+          },
+        ],
+      },
+    });
+
+    // when
+    const result = shouldDisableTime(selectedDate, 'minutes');
+
+    // then
+    expect(result).toEqual(true);
+  });
 });
--- bot-pilot-chat/frontend/src/components/ChatMessage/Accessories/utils/calendar.accessory.utils.ts
+++ bot-pilot-chat/frontend/src/components/ChatMessage/Accessories/utils/calendar.accessory.utils.ts
@@ -1,16 +1,27 @@
 import dayjs, { type Dayjs } from 'dayjs';
 import type { TimeView } from '@mui/x-date-pickers';
-import type { BusyEvent } from '../../../../domain/BusyEvent.ts';
+import useCalendarDetails from '../useCalendarDetails.ts';
 
-export const shouldDisableTime = (value: Dayjs, view: TimeView, busyEvents: BusyEvent[]) => {
+export const shouldDisableTime = (value: Dayjs, view: TimeView) => {
   // only hours and minutes are considered
   if (view !== 'hours' && view !== 'minutes') {
     return false;
   }
+  const { busyEvents, openingHours } = useCalendarDetails();
+  const notInOpeningHours = openingHours.some(
+    (open) =>
+      value.isAfter(open.end.dateTime) ||
+      value.isBefore(open.start.dateTime) ||
+      // do not allow an event at the same time as the end of opening hours
+      value.isSame(open.end.dateTime)
+  );
+  if (notInOpeningHours) {
+    return true;
+  }
   const busyStart = dayjs(value).startOf('minutes');
 
   const busyEnd = busyStart.add(1, 'minutes');
-  return busyEvents.some(
+  return busyEvents.busyEvents.some(
     (event) =>
       dayjs(event.start.dateTime).isBefore(busyEnd) && dayjs(event.end.dateTime).isAfter(busyStart)
   );
