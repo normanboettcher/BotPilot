552a5b5486f0e518e9bd64d9c505749a582de8ff
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-11-23 17:26:26 +0100

    Added - Containerfile for bot_connectors

--- bot-pilot-chat/backend/bot-connectors/src/bot_connectors/config.py
+++ bot-pilot-chat/backend/bot-connectors/src/bot_connectors/config.py
@@ -1,7 +1,5 @@
 import os.path
 
-from dotenv import dotenv_values
-
 
 def get_config():
-    return dotenv_values(os.path.join(os.path.dirname(__file__), "..", "..", ".env"))
+    return {'DATABASE_URL': os.getenv('DATABASE_URL')}
